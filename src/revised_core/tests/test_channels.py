ea93b43d0be8b205448b60c02b6236dffe3c024d
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 10:43:27 -0400

    add test_channels.py

--- src/revised_core/tests/test_channels.py
+++ src/revised_core/tests/test_channels.py
@@ -9,7 +9,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-
+# TODO fix imports
 import unittest
 import core.serializer as serializer
 import core.channels as channels
@@ -117,6 +117,7 @@ class TestChannels(unittest.TestCase):
         while not oc.is_empty():
             b = oc.consume_bit()
             ic.consume_bit(b)
+            # TODO fix len(test_string)
         self.assertEqual(something_read[0], len(test_string))
 
     def testOverwrittingConsistency(self):
