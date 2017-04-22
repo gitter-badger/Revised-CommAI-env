89ad7067b35b70b0a46b91ec148778dd376a1600
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 07:38:28 -0400

    add __inti__.py

---
+++ src/learners/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2017-, Stephen B. Hope
+# All rights reserved.
+#
+# CommAi-env Source code Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+# import all learners in the current directory
+# adapted from: http://stackoverflow.com/a/1057534/367489
+# FIXME: this architecture is probably fragile. Test where it fails and fix it.
+
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__) + "/*.py")
+for m in [basename(f)[:-3] for f in modules if isfile(f)]:
+    __import__('learners.' + m)
