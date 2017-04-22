b1ff727aa1341df922b93b4f73a631a73058f824
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 19:00:23 -0400

    messages.py

--- src/tasks/competition/messages.py
+++ src/tasks/competition/messages.py
@@ -13,36 +13,31 @@
 import random
 
 # a list of congratulations messages to be issued when the learner solves a task
-congratulations = ['good job.',
-                   'bravo.',
-                   'congratulations.',
-                   'nice work.',
-                   'correct.']
+congratulations = ['good job.', 'bravo.', 'congratulations.', 'nice work.', 'correct.']
 # a list of congratulations messages to be issued when the learner fails a task
-failed = ['wrong!',
-          'wrong.',
-          'you failed!',
-          'incorrect.']
+failed = ['wrong!', 'wrong.', 'you failed!', 'incorrect.']
 
 timeout = ['time is out!', 'sorry, time is out!', 'too bad, time out!']
 
 # handy list with word transcriptions of the integers from 0 to 10
-numbers_in_words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six',
-                    'seven', 'eight', 'nine', 'ten']
-
+numbers_in_words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
 
 def number_to_string(num):
-    '''
-    Returns a string version of a number, randomly picking between
-    letters and numbers.
-    '''
+    """ Returns a string version of a number, randomly picking betweenletters and numbers.
+
+    :param num:
+    :return:
+    """
     return random.choice(number_to_strings(num))
 
 
 def number_to_strings(num):
-    '''
-    Returns all the string versions of a number.
-    '''
+    """ Returns all the string versions of a number.
+
+    :param num:
+    :return:
+    """
+
     ret = [str(num)]
     if num <= len(numbers_in_words):
         ret.append(numbers_in_words[num])
@@ -50,17 +45,27 @@ def number_to_strings(num):
 
 
 def string_to_number(n):
+    """
+
+    :param n:
+    :return:
+    """
     if n in numbers_in_words:
         return numbers_in_words.index(n)
     else:
         return int(n)
 
-#
-# simple grammatical functions
-#
+"""
+simple grammatical functions
+"""
 
 
 def indef_article(x):
+    """
+
+    :param x:
+    :return:
+    """
     if x[0] in 'aeiou':
         return 'an ' + x
     else:
@@ -68,6 +73,12 @@ def indef_article(x):
 
 
 def pluralize(obj, c):
+    """
+
+    :param obj:
+    :param c:
+    :return:
+    """
     if c == 1:
         return obj
     else:
@@ -75,8 +86,12 @@ def pluralize(obj, c):
 
 
 def lemmatize(word):
-    # if the word ends with an s and it's the result of pluralization
-    # remove the s:
+    """ if the word ends with an s and it's the result of pluralization remove the s:
+
+    :param word:
+    :return:
+    """
+    #
     if word[-1] == 's' and pluralize(word[:-1], 2) == word:
         return word[:-1]
     else:
