197fd6b40fb52fcdcb8d6343f81dfdbce9bffead
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 10:37:22 -0400

    add __init__

---
+++ src/revised_core/tests/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2017-, Stephen B. Hope
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
