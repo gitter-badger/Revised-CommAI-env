761b8bd1ea3d92e95f752ad424e7e7d463561ca8
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 18:43:06 -0400

    refactor and docstring

--- src/revised_core/tests/test_task.py
+++ src/revised_core/tests/test_task.py
@@ -10,35 +10,66 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 import unittest
 import core.task as task
 
 
 class TestEvents(unittest.TestCase):
+    """
+
+    """
 
     def testTriggers(self):
+        """
+
+        :return:
+        """
         class TestTask(task.Task):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(TestTask, self).__init__(*args, **kwargs)
 
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
             @task.on_message()
             def message_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
             @task.on_timeout()
             def timeout_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
             @task.on_ended()
             def ended_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
         tt = TestTask(max_time=10)
@@ -58,21 +89,51 @@ class TestEvents(unittest.TestCase):
         self.assertEqual(task.Ended, types[self.get_func(TestTask.ended_handler)])
 
     def testInheritance(self):
+        """
+
+        :return:
+        """
         class BaseTask(task.Task):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(BaseTask, self).__init__(*args, **kwargs)
 
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
         class ConcreteTask(BaseTask):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(ConcreteTask, self).__init__(*args, **kwargs)
 
             # overridden handler
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
         tt = ConcreteTask(max_time=10)
@@ -84,13 +145,36 @@ class TestEvents(unittest.TestCase):
         self.assertFalse(self.get_func(BaseTask.start_handler) in handlers)
 
     def testDynamicHandlers(self):
+        """
+
+        :return:
+        """
         class TestTask(task.Task):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(TestTask, self).__init__(*args, **kwargs)
 
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 def end_handler(self, event):
+                    """
+
+                    :param self:
+                    :param event:
+                    :return:
+                    """
                     pass
                 self.end_handler_func = end_handler
                 self.add_handler(task.on_ended()(end_handler))
@@ -98,15 +182,33 @@ class TestEvents(unittest.TestCase):
         triggers = []
         tt = TestTask(max_time=10)
 
-        class EnvironmentMock():
+        class EnvironmentMock:
+            """
+
+            """
             def __init__(self, triggers):
+                """
+
+                :param triggers:
+                """
                 self.triggers = triggers
 
             def raise_event(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 # we only generate an init event
                 tt.start_handler(event)
 
             def _register_task_trigger(self, task, trigger):
+                """
+
+                :param task:
+                :param trigger:
+                :return:
+                """
                 self.triggers.append(trigger)
 
         # raise the start event
@@ -118,6 +220,11 @@ class TestEvents(unittest.TestCase):
         self.assertIn(self.get_func(tt.end_handler_func), handlers)
 
     def get_func(self, method):
+        """
+
+        :param method:
+        :return:
+        """
         try:
             return method.im_func
         except AttributeError: # Python 3
