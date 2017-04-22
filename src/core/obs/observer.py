96bc4b56f7b447f6943e5a6fbb701f989a435c50
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 06:09:12 -0400

    add observer.py

---
+++ src/core/obs/observer.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+class Observable(object):
+    """
+    Simple implementation of the observer pattern
+    """
+    def __init__(self):
+        """
+
+        """
+        self.observers = []
+
+    def register(self, callback):
+        """
+
+        :param callback:
+        :return:
+        """
+        self.observers.append(callback)
+
+    def deregister(self, callback):
+        """
+
+        :param callback:
+        :return:
+        """
+        self.observers.remove(callback)
+
+    def __call__(self, *args):
+        """
+
+        :param args:
+        :return:
+        """
+        for c in self.observers:
+            c(*args)
