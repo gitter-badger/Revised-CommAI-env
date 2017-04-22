686787c89e03d8905b71a8fb4250b7127e839857
Stephen Hope <stepehenbhope@gmail.com>
2017-04-22 05:15:16 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2017-, Stephen B. Hope
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = dict(description='A standard environment for machine learning',
+              author='Stephen Hope',
+              url='https://github.com/stephenbhope/Revised-CommAI-env',
+              download_url='https://github.com/stephenbhope/Revised-CommAI-env',
+              author_email='stephenbhope@gmail.com.',
+              version='0.1',
+              install_requires=['nose'],
+              packages=['NAME'],
+              scripts=[],
+              name='Revised CommAI-env')
+
+setup(**config)
