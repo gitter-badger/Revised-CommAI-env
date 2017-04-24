46ead8d99cef7840d51db6705ac30aab95bbdb29
Stephen Hope <stepehenbhope@gmail.com>
2017-04-24 10:26:10 -0400

    clean

--- src/revised_core/tests/test_session.py
+++ src/revised_core/tests/test_session.py
@@ -6,9 +6,8 @@
 # CommAI-env source files, Copyright (c) 2016-present, Facebook, Inc.
 # All rights reserved.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this
+# source tree. An additional grant of patent rights can be found in the PATENTS file in the same directory.
 
 # TODO fix imports
 import unittest
@@ -55,8 +54,7 @@ class EnvironmentMock(object):
         :return:
         """
         self.task_updated(self.task)
-        # always return a reward of 1
-        return token, 1
+        return token, 1  # always return a reward od 1
 
     def raise_event(self, event):
         # TODO static
@@ -151,6 +149,5 @@ class TestSession(unittest.TestCase):
             if t >= 20:
                 s.stop()
         s.total_time_updated.register(on_time_updated)
-
         s.run()
         self.assertLessEqual(s._total_reward, 10)
