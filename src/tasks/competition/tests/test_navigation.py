ec47e78da87ba0bf793870567c170e356d806a21
Stephen Hope <stepehenbhope@gmail.com>
2017-04-24 17:37:49 -0400

    fix license ref

--- src/tasks/competition/tests/test_navigation.py
+++ src/tasks/competition/tests/test_navigation.py
@@ -4,7 +4,7 @@
 #
 # CommAI-env source files, Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
 #
-# This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this
+# This source code is licensed under the BSD-style license found in the LICENSE.md file in the root directory of this
 # source tree. An additional grant of patent rights can be found in the PATENTS file in the same directory.
 
 
