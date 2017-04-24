ee9e0b7dfc64a06aa6c7329d066cf645074bd66c
Stephen Hope <stepehenbhope@gmail.com>
2017-04-24 17:32:15 -0400

    clean

--- src/tasks/competition/tests/test_navigation.py
+++ src/tasks/competition/tests/test_navigation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2017-, Stephen B. Hope
-# All rights reserved.
+# Copyright (c) 2017-, Stephen B. Hope. All rights reserved.
 #
-# CommAI-env source files, Copyright (c) 2016-present, Facebook, Inc.
-# All rights reserved.
+# CommAI-env source files, Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this
 # source tree. An additional grant of patent rights can be found in the PATENTS file in the same directory.
@@ -25,83 +23,71 @@ class TestNavigation(unittest.TestCase):
     """
     def check_positive_feedback(self, m, instructions):
         # TODO instructions not used
-        """
+        """ hear the congratulations. there is some feedback
 
         :param m:
         :param instructions:
         :return:
         """
-        # hear the congratulations
         feedback_blen = m.read()
-        # there is some feedback
         self.assertGreater(feedback_blen, 0)
         m.send()
         self.assertEqual(m.get_cumulative_reward(), 1)
 
     def check_negative_feedback(self, m):
-        """ hear the feedback
+        """ hear the feedback. it should be bad-learner sort of feedback
 
         :param m:
         :return:
         """
         m.read()
         feedback = m.get_last_message()
-        # it should be bad-learner sort of feedback
         self.assertTrue(feedback in msg.failed or feedback in msg.timeout,
                         'Unexpected negative feedback: {0}'.format(feedback))
 
     def solve_correctly_test(self, m, solve):
-        """ read the instructions
+        """ read the instructions. get the instructions. solve the problem. hear the congratulations
 
         :param m:
         :param solve:
         :return:
         """
         m.read()
-        # get the instructions
         instructions = m.get_last_message()
-        # solve the problem
         solve(m)
-        # hear the congratulations
+        #
         self.check_positive_feedback(m, instructions)
 
     def timeout_test(self, m, solve):
-        # TODO fix solve
-        """
+        # TODO solve not used
+        """ read the instructions. stay silent.  hear the correction
 
         :param m:
         :param solve:
         :return:
         """
-        # read the instructions
         m.read()
-        # stay silent
         while m.is_silent():
             m.send()
-        # hear the correction
         self.check_negative_feedback(m)
 
     def do_test_battery(self, task, solve):
-        """
+        """ test for solving the task correctly. test for not solving it at all
 
         :param task:
         :param solve:
         :return:
         """
         with task_messenger(task, GridWorld) as m:
-            # test for solving the task correctly
             self.solve_correctly_test(m, solve)
         with task_messenger(task, GridWorld) as m:
-            # test for not solving it at all
             self.timeout_test(m, solve)
 
-    #
-    # tasks testing routines
-    #
-
-    # this has been moved into repetition
+"""
+Tasks testing routines
+"""
     def testVerbTask(self):
-        """
+        """ this has been moved into repetition
 
         :return:
         """
@@ -111,11 +97,9 @@ class TestNavigation(unittest.TestCase):
             :param m:
             :return:
             """
-            #
             verb, = m.search_last_message(r"'I (\w+)'")
             m.send("I {verb}.".format(verb=verb))
-        self.do_test_battery(repetition.VerbTask,
-                             solve)
+        self.do_test_battery(repetition.VerbTask, solve)
 
     def testTurningTask(self):
         """
@@ -130,8 +114,7 @@ class TestNavigation(unittest.TestCase):
             """
             direction, = m.search_last_message(r"Turn (\w+)")
             m.send("I turn {direction}.".format(direction=direction))
-        self.do_test_battery(navigation.TurningTask,
-                             solve)
+        self.do_test_battery(navigation.TurningTask, solve)
 
     def testMoving(self):
         """
@@ -145,41 +128,36 @@ class TestNavigation(unittest.TestCase):
             :return:
             """
             m.send("I move forward.")
-        self.do_test_battery(navigation.MovingTask,
-                             solve)
+        self.do_test_battery(navigation.MovingTask, solve)
 
     def testMovingRelative(self):
         """
 
         """
         def solve(m):
-            """
+            """ wait for feedback.  move
 
             :param m:
             :return:
             """
             direction, = m.search_last_message(r"Move (\w+)")
             m.send("I turn {direction}.".format(direction=direction))
-            # wait for feedback
             m.read()
-            # move
             m.send("I move forward.")
-        self.do_test_battery(navigation.MovingRelativeTask,
-                             solve)
+        self.do_test_battery(navigation.MovingRelativeTask, solve)
 
     def testMovingAbsolute(self):
+        # TODO function to lower case
         """
 
         :return:
         """
         def solve(m):
-            """
-
+            """ wait for feedback. move
             :param m:
             :return:
             """
-            init_direction, dest_direction = m.search_last_message(
-                r"facing (\w+), move (\w+)")
+            init_direction, dest_direction = m.search_last_message(r"facing (\w+), move (\w+)")
             directions = ['north', 'east', 'south', 'west']
             init_dir_idx = directions.index(init_direction)
             dest_dir_idx = directions.index(dest_direction)
@@ -192,12 +170,9 @@ class TestNavigation(unittest.TestCase):
                 direction = "left"
             for i in range(abs(delta)):
                 m.send("I turn {direction}.".format(direction=direction))
-                # wait for feedback
                 m.read()
-            # move
             m.send("I move forward.")
-        self.do_test_battery(navigation.MovingAbsoluteTask,
-                             solve)
+        self.do_test_battery(navigation.MovingAbsoluteTask, solve)
 
     def testPickUp(self):
         """
@@ -212,8 +187,7 @@ class TestNavigation(unittest.TestCase):
             """
             object_, = m.search_last_message(r"Pick up the (\w+)")
             m.send("I pick up the {object}.".format(object=object_))
-        self.do_test_battery(navigation.PickUpTask,
-                             solve)
+        self.do_test_battery(navigation.PickUpTask, solve)
 
     def testPickUpAround(self):
         """
@@ -221,13 +195,12 @@ class TestNavigation(unittest.TestCase):
         :return:
         """
         def solve(m):
-            """
+            """ wait for feedback. pick up the object
 
             :param m:
             :return:
             """
-            dest_direction, object_ = m.search_last_message(
-                r"(\w+) from you, pick up the (\w+)")
+            dest_direction, object_ = m.search_last_message(r"(\w+) from you, pick up the (\w+)")
             directions = ['north', 'east', 'south', 'west']
             init_dir_idx = directions.index('north')
             dest_dir_idx = directions.index(dest_direction)
@@ -240,12 +213,9 @@ class TestNavigation(unittest.TestCase):
                 direction = "left"
             for i in range(abs(delta)):
                 m.send("I turn {direction}.".format(direction=direction))
-                # wait for feedback
                 m.read()
-            # pick up the object
             m.send("I pick up the {object}.".format(object=object_))
-        self.do_test_battery(navigation.PickUpAroundTask,
-                             solve)
+        self.do_test_battery(navigation.PickUpAroundTask, solve)
 
     def testPickUpInFront(self):
         """
@@ -253,22 +223,18 @@ class TestNavigation(unittest.TestCase):
         :return:
         """
         def solve(m):
-            """
+            """ wait for feedback.  move
 
             :param m:
             :return:
             """
-            nsteps, object_ = m.search_last_message(
-                r"(\w+) steps forward, pick up the (\w+)")
+            nsteps, object_ = m.search_last_message(r"(\w+) steps forward, pick up the (\w+)")
             nsteps = msg.string_to_number(nsteps)
             for i in range(nsteps - 1):
                 m.send("I move forward.")
-                # wait for feedback
                 m.read()
-            # move
             m.send("I pick up the {object}.".format(object=object_))
-        self.do_test_battery(navigation.PickUpInFrontTask,
-                             solve)
+        self.do_test_battery(navigation.PickUpInFrontTask, solve)
 
     def testGiving(self):
         """
@@ -282,10 +248,8 @@ class TestNavigation(unittest.TestCase):
             :return:
             """
             article, object_ = m.search_last_message(r"I gave you (\w+) (\w+)")
-            m.send("I give you {article} {object}.".format(article=article,
-                                                           object=object_))
-        self.do_test_battery(navigation.GivingTask,
-                             solve)
+            m.send("I give you {article} {object}.".format(article=article, object=object_))
+        self.do_test_battery(navigation.GivingTask, solve)
 
     def testPickUpAroundAndGive(self):
         """
@@ -293,13 +257,12 @@ class TestNavigation(unittest.TestCase):
         :return:
         """
         def solve(m):
-            """
+            """ wait for feedback
 
             :param m:
             :return:
             """
-            article, object_, dest_direction = m.search_last_message(
-                r"There is (\w+) (\w+) (\w+)")
+            article, object_, dest_direction = m.search_last_message(r"There is (\w+) (\w+) (\w+)")
             directions = ['north', 'east', 'south', 'west']
             init_dir_idx = directions.index('north')
             dest_dir_idx = directions.index(dest_direction)
@@ -312,14 +275,11 @@ class TestNavigation(unittest.TestCase):
                 direction = "left"
             for i in range(abs(delta)):
                 m.send("I turn {direction}.".format(direction=direction))
-                # wait for feedback
                 m.read()
             m.send("I pick up the {object}.".format(object=object_))
             m.read()
-            m.send("I give you {article} {object}.".format(article=article,
-                                                           object=object_))
-        self.do_test_battery(navigation.PickUpAroundAndGiveTask,
-                             solve)
+            m.send("I give you {article} {object}.".format(article=article, object=object_))
+        self.do_test_battery(navigation.PickUpAroundAndGiveTask, solve)
 
     def testCountingInventoryGiving(self):
         """
@@ -327,27 +287,24 @@ class TestNavigation(unittest.TestCase):
         :return:
         """
         def solve(m):
-            """
+            """ I don't have anything in the beginning. The teacher just gave me somthing Give the object back to the
+            teacher. I don't have anything anymore
 
             :param m:
             :return:
             """
+            # TODO object named object ............
             object_, = m.search_last_message(r"How many (\w+)")
-            # I don't have anything in the beginning
             m.send(msg.number_to_string(0) + ".")
             m.read()
             article, object_, = m.search_last_message(r"I gave you (\w+) (\w+)")
-            # The teacher just gave me somthing
             m.send(msg.number_to_string(1) + ".")
             m.read()
-            # Give the object back to the teacher
-            m.send("I give you {article} {object}.".format(article=article,
-                                                           object=object_))
+            m.send("I give you {article} {object}.".format(article=article, object=object_))
             m.read()
-            # I don't have anything anymore
             m.send(msg.number_to_string(0) + ".")
-        self.do_test_battery(navigation.CountingInventoryGivingTask,
-                             solve)
+        self.do_test_battery(navigation.CountingInventoryGivingTask, solve)
+        # TODO statement expected
 
 
 def main():
