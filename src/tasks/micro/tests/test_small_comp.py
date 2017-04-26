d87b8f29dca0b7a063d1794284bdf32389c1b1bf
Stephen Hope <stepehenbhope@gmail.com>
2017-04-26 11:29:38 -0400

    WIP fix imports add and expand TODOs

--- src/tasks/micro/tests/test_small_comp.py
+++ src/tasks/micro/tests/test_small_comp.py
@@ -7,11 +7,11 @@
 # This source code is licensed under the BSD-style license found in the LICENSE.md file in the root directory of this
 # source tree. An additional grant of patent rights can be found in the PATENTS file in the same directory.
 
-# TODO fix imports
+# TODO fix tasks, revised_core, imports
 import unittest
 import tasks.micro.small_comp as small_comp
 from tasks.competition.tests.helpers import task_messenger
-from core.serializer import IdentitySerializer
+from Revised_core.serializer import IdentitySerializer
 
 
 class TestSmallCompTasks(unittest.TestCase):
@@ -30,7 +30,7 @@ class TestSmallCompTasks(unittest.TestCase):
         :return:
         """
         sign = answer_correct and 1 or -1
-        with task_messenger(Task, serializer=IdentitySerializer()) as m:
+        with task_messenger(Task, serializer = IdentitySerializer()) as m:
             n = m._env._max_reward_per_task
             for i in range(n):
                 instructions_blen = m.read()
@@ -68,7 +68,7 @@ unit tests
             return "1" if answer[-1] == "0" else "0"
 
         self._test_solution(small_comp.ReverseXTask, get_correct_answer, True)
-        # TODO staement expected
+        # TODO statement expected
         self._test_solution(small_comp.ReverseXTask, get_incorrect_answer_impatient, False)
 
 
